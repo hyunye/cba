4bdf5ad939e2cafc75e0de4f8d40e40783875f75
CouldBeAncient <chebotaaaaaa@gmail.com>
2026-01-08 22:13:13 +0900

    기본 설정 조짐

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+function a(baka: string) {
+  const sus = 1
+  return baka + sus
+}
+
+console.log(a)
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    dts({ include: ['src'] })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      fileName: () => `cba.js`,
+      formats: ['es'],
+    }
+  }
+})
