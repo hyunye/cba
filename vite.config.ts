a129c435be8a4343bad71e45447c847b34bc5d40
CouldBeAncient <chebotaaaaaa@gmail.com>
2026-01-08 23:38:41 +0900

    개고생

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,34 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
 
 
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    dts({ include: ['src'] })
-  ],
-  build: {
-    minify: "terser",
-    lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      fileName: () => `cba.js`,
-      formats: ['es'],
+export default defineConfig(({ command, mode }) => {
+  const enableWatch = process.argv.includes('--watch')
+  return {
+    server: {
+      cors: true
+    },
+    publicDir: 'dist',
+    plugins: [
+      tsconfigPaths(),
+      dts({ include: ['src'] })
+    ],
+    build: {
+      emptyOutDir: false,
+      minify: "terser",
+      sourcemap: true,
+      lib: {
+        entry: resolve(__dirname, 'src/main.ts'),
+        name: "fastcode",
+        fileName: () => `build.js`,
+        formats: ['iife'],
+      },
+      ...(enableWatch ? {
+        watch: {
+          include: [
+            'src/**'
+          ],
+        }
+      } : {})
     }
   }
 })
